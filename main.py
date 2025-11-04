9164bf682a4f034acc423faff001cad11af9d599
tyson1024 <tysonbellistr@gmail.com>
2025-11-04 15:16:35 -0500

    test

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
+
 def main():
     print("Hello from ai-agent!")
 
+load_dotenv()
+api_key = os.environ.get("AIzaSyD4W94XWwHrt8GZQFeHYoJvzSPVOOYrIaU")
 
 if __name__ == "__main__":
     main()
